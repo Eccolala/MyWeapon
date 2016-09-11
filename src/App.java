ac113e4de9aa47eb5b8567e0285e2a9de7beb398
eccolala <764884989@qq.com>
2016-09-11 09:37:25 +0800

    数据准备完成

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import org.jsoup.Connection;
@@ -10,14 +11,16 @@ import org.jsoup.select.Elements;
  * Created by eccolala on 16-9-7.
  */
 
-
 public class App {
     public static void main(String arg[]) throws IOException {
-        String login = "http://202.195.206.35:8080/jsxsd/xk/LoginToXk";
-        String grade = "http://202.195.206.35:8080/jsxsd/kscj/cjcx_list";
-        String table = "http://jwgl.just.edu.cn:8080/jsxsd/xskb/xskb_list.do";
+        final String login = "http://202.195.206.35:8080/jsxsd/xk/LoginToXk";
+        final String grade = "http://202.195.206.35:8080/jsxsd/kscj/cjcx_list";
+        final String table = "http://202.195.206.35:8080/jsxsd/xskb/xskb_list.do";
+
+        ArrayList<ClassBean> classList = new ArrayList<>();
 
         Map<String, String> map = new HashMap<String, String>();
+
         map.put("USERNAME", "1341901213");
         map.put("PASSWORD", "112358gyx");
 
@@ -30,13 +33,90 @@ public class App {
         Map<String, String> cookies = response.cookies();
         Document doc = Jsoup.connect(table)
             .cookies(cookies)
-            .timeout(3000)
             .get();
-        Elements allTd = doc.select("td");
 
-        for (org.jsoup.nodes.Element element : allTd) {
-            String td = element.text();
-            System.out.println(td);
+        Elements tables = doc.select("table[id=kbtable]");
+
+        Elements trs = tables.select("tr");
+
+        //周数
+        for (int tr_index = 0; tr_index < trs.size(); tr_index++) {
+
+            Elements divs = trs.get(tr_index).select("div[class=kbcontent]");
+
+            // System.out.println(divs);
+
+            for (int div_index = 0; div_index < divs.size(); div_index++) {
+
+                String html = divs.get(div_index).outerHtml().replaceAll("<br>", "#~#");
+                Document doc1 = Jsoup.parse(html.toString());
+                String newHtml = doc1.text();
+
+                //去除没有课程信息的课程
+                if (newHtml.length() < 2) {
+                    continue;
+                }
+
+                String[] ary = newHtml.split("#~#");
+
+                //创建一个课程
+                ClassBean bean = new ClassBean();
+
+                //遍历数组
+                for (int i = 0; i < ary.length; i++) {
+
+                    //去除所有空格
+                    ary[i] = ary[i].replaceAll("\\s+", "");
+
+                    //课程坐标
+                    bean.posX = div_index;
+
+                    bean.posY = tr_index;
+
+                    //一种课程选择情况
+                    if (i % 5 == 0) {
+
+                        bean.classNum = ary[i];
+
+                    } else if (i % 5 == 1) {
+
+                        bean.className = ary[i];
+
+                    } else if (i % 5 == 2) {
+
+                        bean.techName = ary[i];
+
+                    } else if (i % 5 == 3) {
+
+                        String time = ary[i];
+
+                        time = time.substring(0, time.length() - 3);
+
+                        String[] dots = time.split(",");
+
+                        bean.classTime = dots;
+
+                    } else if (i % 5 == 4) {
+                        bean.classAddr = ary[i];
+                    }
+
+                    System.out.println(ary[i].replaceAll("\\s+", ""));
+
+                }
+
+                //把课程加入课表
+                classList.add(bean);
+
+            }
+
+        }
+
+        System.out.println(classList.size());
+
+        for (ClassBean bean : classList) {
+
+            System.out.println(bean.posY);
+
         }
 
     }
---
+++ src/ClassBean.java
@@ -0,0 +1,17 @@
+/**
+ * Created by eccolala on 16-9-8.
+ */
+
+//课程实体
+
+public class ClassBean {
+    public String classNum;
+    public String className;
+    public String techName;
+    //上课周次
+    public String[] classTime;
+    public String classAddr;
+    //课程坐标
+    public int posX;
+    public int posY;
+}
